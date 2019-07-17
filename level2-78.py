acd78466b67db1f79434cd73fa08c8ae95ba5cd6
Gutha Bharath Kumar <38840613+gbharatnaidu@users.noreply.github.com>
2019-07-17 19:27:48 +0530

    Create level2-78.py

---
+++ level2-78.py
@@ -0,0 +1,11 @@
+m,n=map(int,input().split())
+lst=input()
+lst=lst.split()
+lst=list(map(int,lst))
+lst1=input()
+lst1=lst1.split()
+lst1=list(map(int,lst1))
+for x in lst1:
+  lst.append(x)
+lst.sort()
+print(lst)
