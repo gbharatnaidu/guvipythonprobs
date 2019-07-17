a2663fd85b50c4fa89863c793a76d70377451790
Gutha Bharath Kumar <38840613+gbharatnaidu@users.noreply.github.com>
2019-07-17 18:52:56 +0530

    Update level2-72.py

--- level2-72.py
+++ level2-72.py
@@ -2,7 +2,7 @@ lst=input()
 lst=lst.split()
 lst=list(map(int,lst))
 for i in range(len(lst)-1):
-  if(lst[i]<lst[i+1]):
-   k=lst[i+1]
+  if(lst[i]>lst[i+1]):
+   k=lst[i]
    break
-print(lst[i+1])
+print(k)
